6cab5a64224ef8f4886597aa9a7a3ccbb88aece4
mralansurfs <mralansurfs@gmail.com>
2024-11-02 22:36:01 +1300

    first

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
 import pandas as pd
 
+def read_csv_data(fname):
+    """
+    Function to read the csv data and return the result as a DataFrame.
+    Args:
+        fname (str): file to read the data from
+    Returns:
+        DataFrame containing the wave buoy data
+    """
+    df = pd.read_csv(fname).set_index('origintime')
+    df.index = pd.to_datetime(df.index)
+    return(df)
